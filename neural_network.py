0e6fbcdb9fcf5d9959df9da88137504c40224c5e
Grzegorz Sikora <gregor.sikora@gmail.com>
2023-12-28 20:40:14 +0100

    backward methods added

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,7 @@
 import numpy as np
 import nnfs
-from cd import create_data
 
 nnfs.init()
-
-X, y = create_data(100, 3)
 
 
 class Layer_Dense:
@@ -12,20 +9,58 @@ class Layer_Dense:
         self.weights = 0.10 * np.random.rand(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
+    # Froward pass
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
 class Activation_ReLU:
+
+    # Forward pass
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
 
+    # Backward pass
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
 
-layer1 = Layer_Dense(2, 5)
-activation1 = Activation_ReLU()
+        # Zero gradient for negative values
+        self.dinputs[self.inputs <=0] = 0
 
-layer1.forward(X)
-print(layer1.output)
 
-activation1.forward(layer1.output)
-print(activation1.output)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
