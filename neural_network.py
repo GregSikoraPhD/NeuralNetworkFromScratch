172f38818f10e73eb2819a94fb129a102f457f14
Grzegorz Sikora <gregor.sikora@gmail.com>
2024-01-12 11:39:58 +0100

    added new objects

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
 
@@ -39,10 +40,34 @@ class Activation_ReLU:
 
 
 class Activation_Softmax:
+
+    # Forward pass
     def forward(self, inputs):
+        # Remember input values
+        self.inputs = inputs
+
+        # Get unnormalized probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+        # Normalize probabilities
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
         self.output = probabilities
+
+    # Backward pass
+    def backward(self, dvalues):
+
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient and add to array
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 
 
 class Loss:
@@ -53,14 +78,143 @@ class Loss:
 
 
 class Loss_CategoricalCrossentropy(Loss):
+
+    # Forward pass
     def forward(self, y_pred, y_true):
+
+        # Number of samples in a batch
         samples = len(y_pred)
+        # Clip data to prevent division by 0
         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
 
+        # Probabilities for target values - only if categorical labels
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
+        # Mask values - only for one-hot encoded labels
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
 
+        # Losses
         negative_log_likelihood = -np.log(correct_confidences)
         return negative_log_likelihood
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+
+    # Creates activation and loss objects
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+
+    # Forward pass
+    def forward(self, inputs, y_true):
+        # Output layer's activation function
+        self.activation.forward(inputs)
+        # Set the output
+        self.output = self.activation.output
+        # Calculate and return loss value
+        return self.loss.calculate(self.output, y_true)
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # If labels are one-hot encoded, turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        # Copy for safe modification
+        self.dinputs = dvalues.copy()
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -= 1
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
+class Optimizer_SGD:
+
+    # Initialize optimizer
+    def __init__(self, learning_rate=1., decay=0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = (self.learning_rate *
+                                          (1. / (1. + self.decay * self.iterations)))
+
+    # Update parameters
+    def update_params(self, layer):
+        layer.weights += -self.learning_rate * layer.dweights
+        layer.biases += -self.learning_rate * layer.dbiases
+
+    def post_update_params(self):
+        self.iterations += 1
+# ===============================Creating objects=====================================
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+
+# Create Dense layer with 2 input features and 64 output values
+dense1 = Layer_Dense(2, 64)
+
+# Create ReLU activation
+activation1 = Activation_ReLU()
+
+# Create second Dense layer
+dense2 = Layer_Dense(64, 3)
+
+# Create softmax classifier combined loss and activation
+loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+
+# Create optimizer
+optimizer = Optimizer_SGD()
+# ====================================Training===========================================
+for epoch in range(10001):
+    # ===================================Forward pass ===================================
+    # Perform a forward pass of the first layer
+    dense1.forward(X)
+
+    # Perform a forward pass of activation ReLU
+    activation1.forward(dense1.output)
+
+    # Perform a forward pass of second layer
+    dense2.forward(activation1.output)
+
+    # Loss
+    loss = loss_activation.forward(dense2.output, y)
+    # ========================Accuracy =================================================
+    predictions = np.argmax(loss_activation.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    accuracy = np.mean(predictions == y)
+    # ====================Printing info about training==================================
+    if not epoch % 100:
+        print(f'epoch: {epoch},' + f'acc: {accuracy:.3f},' + f'loss: {loss:.3f}')
+    # ======================== Backward pass ===========================================
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+    # ============================Update weights and biases========================================
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
